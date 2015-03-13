b754ca5b5cd3ecaae8e1b8bcb1fa8ad48464ccc9
Didit Sepiyanto <sepiyantodidit@gmail.com>
2015-03-14 00:45:43 +0700

    slider saya hilangkan, halaman blog saya edit, gambar saya rubah

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -2,26 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class blog extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	
 	public function index()
 	{
+		$this->load->database();
+		$query = $this->db->query("SELECT * FROM `blog` WHERE 1");
+		$value = $query->row();
+		$judul_blog['judul_blog']=$value->judul_blog;
 		$this->load->view('header');
-		$this->load->view('blog');
+		$this->load->view('blog',$judul_blog);
 		$this->load->view('footer');
 	}
 }
--- application/views/beranda.php
+++ application/views/beranda.php
@@ -1,46 +1,6 @@
-<!-- Bootstrap Core CSS -->
-<link href="<?php echo base_url(); ?>assets/slide/css/bootstrap.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-<link href="<?php echo base_url(); ?>assets/slide/css/full-slider.css" rel="stylesheet">
-<!-- slider -->
-<div id="myCarousel" class="carousel slide" >
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	</ol>
-	<div class="carousel-inner">
-		<div class="item active"> 
-			<!-- Set the first background image using inline CSS below. -->
-			<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-			<div class="carousel-caption">
-				<h4>Caption 1</h4>
-				<h4>Caption 3</h4>
-			</div>
-		</div>
-		<div class="item"> 
-			<!-- Set the second background image using inline CSS below. -->
-			<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-			<div class="carousel-caption">
-				<h4>Caption 1</h4>
-				<h4>Caption 3</h4>
-			</div>
-		</div>
-		<div class="item"> 
-			<!-- Set the third background image using inline CSS below. -->
-			<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-			<div class="carousel-caption">
-				<h4>Caption 1</h4>
-				<h4>Caption 3</h4>
-			</div>
-		</div>
-	</div>
-	
-	<!-- Controls --> 
-	<a class="left carousel-control" href="#myCarousel" data-slide="prev"> <span class="icon-prev"></span> </a> <a class="right carousel-control" href="#myCarousel" data-slide="next"> <span class="icon-next"></span> </a> </div>
-
-<!-- end slider -->
+<div class="parallax-container">
+	<div class="parallax"><img src="<?php echo base_url(); ?>assets/blog/tcode.jpg"></div>
+</div>
 <div class="section no-pad-bot" id="index-banner">
 	<div class="container">
 		<div class="row center">
