23505d92f3df2f773cdb7bd959e82168083815e4
Didit Sepiyanto <sepiyantodidit@gmail.com>
2015-04-02 14:38:56 +0700

    lala po

--- application/controllers/tentang_kami.php
+++ application/controllers/tentang_kami.php
@@ -1,10 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class tentang_kami extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
+		$this->load->view('link');
+		$this->load->view('body_navbar');
 		$this->load->view('tentang_kami');
 		$this->load->view('footer');
 	}
