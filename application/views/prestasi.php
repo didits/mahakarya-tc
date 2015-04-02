4d9b1dd50ff452221963954c42f650e35bf1ccfc
Muhammad Dery Rahma <halohalobandung88@gmail.com>
2015-04-02 16:09:40 +0700

    list prestasi

--- application/views/prestasi.php
+++ application/views/prestasi.php
@@ -8,59 +8,99 @@
 	</ul>
 	<div class="TabbedPanelsContentGroup">
 		<div class="TabbedPanelsContent">
-		<div class="section"> 
-	<!--   Icon Section   -->
-	<div class="row">
-		<div class="col s12 m4">
-			<div class="icon-block">
-				<img src="<?php echo base_url(); ?>assets/blog/tcode.jpg" style="width:100%; height:100%;"/>
-			</div>
-		</div>
-		<div class="col s12 m8">
-			<div class="icon-block">
-				<h5>Tcode Februari 2015</h5>
-				<p><i class="mdi-action-event"></i>March 3, 2015&nbsp;&nbsp;<i class="mdi-action-loyalty"></i><span class="tag">TCode</span></p>
-				<p class="light">Tcode Bulan Februari 2015 telah selesai diselenggarakan. Setelah melalui pertarungan sengit selama 3 jam di Laboratorium Workshop Pemrograman 1 Teknik Informatika, diperoleh pemenang Tcode Februari 2015. Berikut ini daftar pemenangnya.<br />
-- Juara 1 – John Stephanus Peter (5113100106)<br />
-- Juara 2 – Azkaa Khoirudin (5113100135)<br />
-- Peserta Terbaik Angkatan 2014 – Ilham Aulia Majid (5114100144)<br />
+			<div class="section"> 
+				<!--   Icon Section   -->
+				<div class="row">
+					<div class="col s12 m4">
+						<div class="icon-block">
+							<img src="<?php echo base_url(); ?>assets/blog/tcode.jpg" style="width:100%; height:100%;"/>
+						</div>
+					</div>
+					<div class="col s12 m8">
+						<div class="icon-block">
+							<h5>Tcode Februari 2015</h5>
+							<p><i class="mdi-action-event"></i>March 3, 2015&nbsp;&nbsp;<i class="mdi-action-loyalty"></i><span class="tag">TCode</span></p>
+							<p class="light">Tcode Bulan Februari 2015 telah selesai diselenggarakan. Setelah melalui pertarungan sengit selama 3 jam di Laboratorium Workshop Pemrograman 1 Teknik Informatika, diperoleh pemenang Tcode Februari 2015. Berikut ini daftar pemenangnya.<br />
+			- Juara 1 – John Stephanus Peter (5113100106)<br />
+			- Juara 2 – Azkaa Khoirudin (5113100135)<br />
+			- Peserta Terbaik Angkatan 2014 – Ilham Aulia Majid (5114100144)<br />
 
-Selamat kepada seluruh pemenang dan terima kasih kepada seluruh peserta atas partisipasinya. Sampai jumpa di Tcode berikutnya pada Bulan Maret 2015. Untuk informasi lebih lanjut dan pendaftaran Tcode bisa diakses di sini.</p>
+			Selamat kepada seluruh pemenang dan terima kasih kepada seluruh peserta atas partisipasinya. Sampai jumpa di Tcode berikutnya pada Bulan Maret 2015. Untuk informasi lebih lanjut dan pendaftaran Tcode bisa diakses di sini.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-</div>
-<div class="divider"></div>
-<div class="section"> 
-	<!--   Icon Section   -->
-	<div class="row">
-		<div class="col s12 m4">
-			<div class="icon-block">
-				<img src="<?php echo base_url(); ?>assets/blog/tcode.jpg" style="width:100%; height:50%;"/>
-			</div>
-		</div>
-		<div class="col s12 m8">
-			<div class="icon-block">
-				<h5>Tcode Februari 2015</h5>
-				<p><i class="mdi-action-event"></i>March 3, 2015&nbsp;&nbsp;<i class="mdi-action-loyalty"></i><span class="tag">TCode</span></p>
-				<p class="light">Tcode Bulan Februari 2015 telah selesai diselenggarakan. Setelah melalui pertarungan sengit selama 3 jam di Laboratorium Workshop Pemrograman 1 Teknik Informatika, diperoleh pemenang Tcode Februari 2015. Berikut ini daftar pemenangnya.<br />
-- Juara 1 – John Stephanus Peter (5113100106)<br />
-- Juara 2 – Azkaa Khoirudin (5113100135)<br />
-- Peserta Terbaik Angkatan 2014 – Ilham Aulia Majid (5114100144)<br />
+			<div class="divider"></div>
+			<div class="section"> 
+				<!--   Icon Section   -->
+				<div class="row">
+					<div class="col s12 m4">
+						<div class="icon-block">
+							<img src="<?php echo base_url(); ?>assets/blog/tcode.jpg" style="width:100%; height:50%;"/>
+						</div>
+					</div>
+					<div class="col s12 m8">
+						<div class="icon-block">
+							<h5>Tcode Februari 2015</h5>
+							<p><i class="mdi-action-event"></i>March 3, 2015&nbsp;&nbsp;<i class="mdi-action-loyalty"></i><span class="tag">TCode</span></p>
+							<p class="light">Tcode Bulan Februari 2015 telah selesai diselenggarakan. Setelah melalui pertarungan sengit selama 3 jam di Laboratorium Workshop Pemrograman 1 Teknik Informatika, diperoleh pemenang Tcode Februari 2015. Berikut ini daftar pemenangnya.<br />
+			- Juara 1 – John Stephanus Peter (5113100106)<br />
+			- Juara 2 – Azkaa Khoirudin (5113100135)<br />
+			- Peserta Terbaik Angkatan 2014 – Ilham Aulia Majid (5114100144)<br />
 
-Selamat kepada seluruh pemenang dan terima kasih kepada seluruh peserta atas partisipasinya. Sampai jumpa di Tcode berikutnya pada Bulan Maret 2015. Untuk informasi lebih lanjut dan pendaftaran Tcode bisa diakses di sini.</p>
+			Selamat kepada seluruh pemenang dan terima kasih kepada seluruh peserta atas partisipasinya. Sampai jumpa di Tcode berikutnya pada Bulan Maret 2015. Untuk informasi lebih lanjut dan pendaftaran Tcode bisa diakses di sini.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-</div>
-<div class="divider" style="margin-bottom:20px;"></div>
-<div class="row">
-		<div class="center">
-		<a class="waves-effect waves-light btn disabled tooltipped" data-position="bottom" data-delay="50" data-tooltip="Halaman sebelumnya"><i class="mdi-hardware-keyboard-arrow-left"></i></a>
-		<a class="waves-effect waves-light btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Halaman selanjutnya"><i class="mdi-hardware-keyboard-arrow-right"></i></a>
-		</div>
-	</div>
-		</div>
-		<div class="TabbedPanelsContent">Content 2</div>
-	</div>
-</div>
+			<div class="divider" style="margin-bottom:20px;"></div>
+			<div class="row">
+				<div class="center">
+					<a class="waves-effect waves-light btn disabled tooltipped" data-position="bottom" data-delay="50" data-tooltip="Halaman sebelumnya"><i class="mdi-hardware-keyboard-arrow-left"></i></a>
+					<a class="waves-effect waves-light btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Halaman selanjutnya"><i class="mdi-hardware-keyboard-arrow-right"></i></a>
+				</div>
+				</div>
+					</div>
+					<div class="TabbedPanelsContent">
+						<center><span><b>DATABASE PRESTAST TEKNIK INFORMATIKA ITS</b></span></center>
+						<table class="bordered hoverable">
+							<thead>
+						      <tr>
+						        <th>Nomor</th>
+						        <th>Nama Kompetisi</th>
+						        <th>Waktu</th>
+						        <th>Bidang</th>
+						        <th>Ruang Lingkup</th>
+						        <th>Peringkat</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						      <tr>
+						        <td>John</td>
+						        <td>Doe</td>
+						        <td>john@example.com</td>
+						        <td>John</td>
+						        <td>Doe</td>
+						        <td>john@example.com</td>
+						      </tr>
+						      <tr>
+						        <td>Mary</td>
+						        <td>Moe</td>
+						        <td>mary@example.com</td>
+						        <td>Mary</td>
+						        <td>Moe</td>
+						        <td>mary@example.com</td>
+						      </tr>
+						      <tr>
+						        <td>July</td>
+						        <td>Dooley</td>
+						        <td>july@example.com</td>
+						        <td>July</td>
+						        <td>Dooley</td>
+						        <td>july@example.com</td>
+						      </tr>
+						    </tbody>
+						</table>
+					</div>
+				</div>
+			</div>
