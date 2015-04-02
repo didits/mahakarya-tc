8e9599ba93df82be570a379ea9ed256f08acd450
hanumuslem <hanumuslem@gmail.com>
2015-04-02 14:37:31 +0700

    MEMBUAT ADMIN PRESTASI

---
+++ application/views/body_navbaradmin.php
@@ -0,0 +1,24 @@
+<body style="background-color:#f2f2f2">
+<div class="navbar-fixed" style="position:fixed;z-index:1000;top:0;">
+	<nav class="lighten-1" role="navigation" style="background-color:#1a1a1a;">
+		<div class="row" >
+			<div class="col s12">
+				<div class="nav-wrapper"><span style="margin-left:10px; position:relative; top:7px; margin-right:10px;"><img src="<?php echo base_url(); ?>assets/logo/logo.svg" width="50px"/></span><span style="color:#00ffff; font-size:18px;">Admin</span>
+					<ul id="nav-mobile" class="right side-nav">
+						<li class="waves-effect waves-teal"><a href="<?php echo base_url(); ?>">Beranda</a></li>
+						<li><!-- Dropdown Trigger -->
+  <a class='dropdown-button' href='#' data-activates='dropdown1'>Mahakarya</a>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">Tugas Akhir</a></li>
+    <li><a href="#!">Final Project</a></li>
+  </ul></li>
+						<li class="waves-effect waves-teal"><a href="<?php echo base_url(); ?>index.php/prestasi">Prestasi</a></li>
+						<li class="waves-effect waves-teal"><a href="<?php echo base_url(); ?>index.php/tentang_kami">Tentang Kami</a></li>
+					</ul>
+					<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a> </div>
+			</div>
+		</div>
+	</nav>
+</div>
